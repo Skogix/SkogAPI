1c88205f3f6d4d257bf9de6acb8cc6df3ad73dde
Skogix <Skogix@users.noreply.github.com>
2025-02-20 19:01:37 +0000

    ✅ Ready to clone and code.

--- skogapi/app.py
+++ skogapi/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+skogapi API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="skogapi",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://skogapi.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "Skogix",
+        "url": "http://skogapi.com/contact/",
+        "email": "Skogix@skogapi.com",
     },
     license_info={
         "name": "The Unlicense",
